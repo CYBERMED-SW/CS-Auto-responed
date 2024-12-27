38c6d0fbf52370f34f1b55c09a48feee4661ad4a
LEEWONJIN-3 <wjlee@cybermed.co.kr>
2024-12-27 11:33:24 +0900

    241227l

--- .vscode/learn.py
+++
@@ -1,7 +0,0 @@
-# 데이터 분리
-from sklearn.model_selection import train_test_split
-
-X_train, X_val, y_train, y_val = train_test_split(question_padded, answer_padded, test_size=0.2)
-
-# 학습
-model.fit([X_train, y_train[:, :-1]], y_train[:, 1:], epochs=10, validation_data=([X_val, y_val[:, :-1]], y_val[:, 1:]))
